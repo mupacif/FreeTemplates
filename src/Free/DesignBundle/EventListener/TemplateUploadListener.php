b624520621d2377e66499ee3868cfa3250eee4d0
Pacific <mupacif@gmail.com>
2016-12-07 14:30:11 +0100

    Version 1

---
+++ src/Free/DesignBundle/EventListener/TemplateUploadListener.php
@@ -0,0 +1,50 @@
+<?php
+namespace Free\DesignBundle\EventListener;
+
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
+use AppBundle\Entity\Product;
+use AppBundle\FileUploader;
+
+class TemplatesUploadListener
+{
+    private $uploader;
+
+    public function __construct(FileUploader $uploader)
+    {
+        $this->uploader = $uploader;
+    }
+
+    public function prePersist(LifecycleEventArgs $args)
+    {
+        $entity = $args->getEntity();
+
+        $this->uploadFile($entity);
+    }
+
+    public function preUpdate(PreUpdateEventArgs $args)
+    {
+        $entity = $args->getEntity();
+
+        $this->uploadFile($entity);
+    }
+
+    private function uploadFile($entity)
+    {
+        // upload only works for Product entities
+        if (!$entity instanceof Template) {
+            return;
+        }
+
+        $file = $entity->getTumbnail();
+
+        // only upload new files
+        if (!$file instanceof UploadedFile) {
+            return;
+        }
+
+        $fileName = $this->uploader->upload($file);
+        $entity->setTumbnail($fileName);
+    }
+}
