2572ecdebd19cb4a4da4fc26822f743f1c3ecade
Pacific <mupacif@gmail.com>
2016-12-07 23:03:07 +0100

    added console

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,9 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Free\DesignBundle\FreeDesignBundle(),
+            new CoreSphere\ConsoleBundle\CoreSphereConsoleBundle(),
             new Free\UserBundle\FreeUserBundle(),
+
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
@@ -25,6 +27,7 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            
         }
 
         return $bundles;
