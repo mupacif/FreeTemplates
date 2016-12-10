beda6e6fc8766bb217fa939f82d29158ed626043
Pacific <mupacif@gmail.com>
2016-12-10 23:29:05 +0100

    Added widget Controller and linked views, and showed it in add Category page

---
+++ src/Free/DesignBundle/Controller/WidgetsController.php
@@ -0,0 +1,22 @@
+<?php
+namespace Free\DesignBundle\Controller;
+
+        use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+        class WidgetsController extends Controller
+        {
+        	                   public function categoriesAction()
+                        {
+                         $repository = $this
+                        ->getDoctrine()
+                        ->getManager()
+                        ->getRepository('FreeDesignBundle:Category');
+
+                         $categories= $repository->findAll();
+                            return $this->render(
+                        'FreeDesignBundle:widgets:showCategories.html.twig',
+                     array('categories' => $categories)
+                         );
+
+                        }
+        }
