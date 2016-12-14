bd0dd9a2b1e4bba06a8498d26958d3cdab2a01e9
Pacific <mupacif@gmail.com>
2016-12-14 16:16:38 +0100

    correctess

--- src/Free/DesignBundle/Controller/WidgetsController.php
+++ src/Free/DesignBundle/Controller/WidgetsController.php
@@ -4,19 +4,19 @@ namespace Free\DesignBundle\Controller;
         use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
         class WidgetsController extends Controller
-        {
-        	                   public function categoriesAction()
-                        {
-                         $repository = $this
-                        ->getDoctrine()
-                        ->getManager()
-                        ->getRepository('FreeDesignBundle:Category');
+            {
+            	                   public function categoriesAction()
+                            {
+                             $repository = $this
+                            ->getDoctrine()
+                            ->getManager()
+                            ->getRepository('FreeDesignBundle:Category');
 
-                         $categories= $repository->findAll();
-                            return $this->render(
-                        'FreeDesignBundle:widgets:showCategories.html.twig',
-                     array('categories' => $categories)
-                         );
+                             $categories= $repository->findAll();
+                                return $this->render(
+                            'FreeDesignBundle:widgets:showCategories.html.twig',
+                             array('categories' => $categories)
+                             );
 
-                        }
-        }
+                            }
+            }
